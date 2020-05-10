4b05fa753edb35e731b425cbb23eb7947712fb21
jearbot <heskettjeremy@gmail.com>
2020-05-09 22:12:22 -0500

    shit

--- app/jobs/run_neato_job.rb
+++ app/jobs/run_neato_job.rb
@@ -4,9 +4,10 @@ class RunNeatoJob < ApplicationJob
   require 'net/http'
   require 'uri'
   require 'json'
+  require 'oauth'
 
-  # SERIAL = Rails.application.config.serial_number.freeze
-  # SECRET = Rails.application.config.secret.freeze
+  # Helpful website
+  # https://forum.logicmachine.net/showthread.php?tid=660
 
   SERIAL = Rails.application.config.serial_number.freeze
   SECRET = Rails.application.config.secret.freeze
@@ -28,43 +29,6 @@ class RunNeatoJob < ApplicationJob
       },
       body: '{"reqId":"1","cmd":"startCleaning","params":{"category":2,"mode":2,"navigationMode":1}}'
     )
-
-
-
-
-    # uri = URI.parse("https://nucleo.neatocloud.com:4443/vendors/neato/robots/#{RunNeatoJob::SERIAL}/messages")
-    # request = Net::HTTP::Post.new(uri)
-    # request.content_type = "application/x-www-form-urlencoded; charset=UTF-8"
-    # request["Connection"] = "keep-alive"
-    # request["Accept"] = "application/vnd.neato.nucleo.v1"
-    # request["Authorization"] = "NEATOAPP " + @token
-    # request["X-Date"] = "Sat, 02 May 2020 01:36:39 GMT"
-    # request["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36"
-    # request["Origin"] = "https://developers.neatorobotics.com"
-    # request["Sec-Fetch-Site"] = "cross-site"
-    # request["Sec-Fetch-Mode"] = "cors"
-    # request["Sec-Fetch-Dest"] = "empty"
-    # request["Referer"] = "https://developers.neatorobotics.com/demo/sdk-js/index.html"
-    # request["Accept-Language"] = "en-US,en;q=0.9"
-    request.body = JSON.dump({
-      "reqId" => "1",
-      "cmd" => "startCleaning",
-      "params" => {
-        "category" => 2,
-        "mode" => 2,
-        "navigationMode" => 1
-      }
-    })
-
-    # req_options = {
-    #   use_ssl: uri.scheme == "https",
-    # }
-
-    # response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-    #   http.request(request)
-    # end
-
-    # puts response.body
   end
 
   private
