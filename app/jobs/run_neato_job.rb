a7a14cc4b88dfc5e24f198f66dbed5a1faee1c18
jearbot <heskettjeremy@gmail.com>
2020-06-02 18:37:36 -0500

    improvements

--- app/jobs/run_neato_job.rb
+++ app/jobs/run_neato_job.rb
@@ -16,71 +16,83 @@ class RunNeatoJob < ApplicationJob
   REFRESH_TOKEN = RefreshToken.last.key.freeze
 
   def perform
-    # oauth2
-    # get_token
+    @date = Time.now.utc.strftime("%a, %d %b %Y %H:%M:%S GMT")
 
+    get_signature
+    response = HTTParty.post("https://nucleo.neatocloud.com:4443/vendors/neato/robots/#{ROBOT_SERIAL}/messages",
+      headers: {
+        'Accept' => 'application/vnd.neato.nucleo.v1',
+        'Date' => @date,
+        'Authorization' =>  "NEATOAPP " + @signature,
+      },
+      body: JSON.dump({ reqId: "13", cmd: "startCleaning", params: { category: 2, mode: 2, modifier: 1 }})
+    )
+  end
+
+  def get_signature
+    body = JSON.dump({ reqId: "13", cmd: "startCleaning", params: { category: 2, mode: 2, modifier: 1 }})
+
+    string_to_sign = "#{ROBOT_SERIAL.downcase}\n#{@date}\n#{body}"
+
+    @signature = OpenSSL::HMAC.hexdigest('sha256', ROBOT_SECRET, string_to_sign)
+  end
+
+  def get_robot_secret_key
     response = HTTParty.get("#{NEATO_ENDPOINT}/users/me/robots",
       headers: {
         'Accept' => HEADER_URL,
         'Authorization' =>  "Bearer " + "#{ACCESS_TOKEN}",
       }
     )
-    body = '{"reqId":"77", "cmd":"getRobotState"}'
+    body = '{"reqId":"1", "cmd":"getRobotState"}'
+  end
 
-    response = HTTParty.post("https://nucleo.neatocloud.com:4443/vendors/neato/robots/#{ROBOT_SERIAL}/messages",
+  def get_map_id
+    response = HTTParty.get("#{NEATO_ENDPOINT}/users/me/robots/#{ROBOT_SERIAL}/maps",
       headers: {
-        'Accept' => 'application/vnd.neato.nucleo.v1',
-        'Date' => date = Time.now.utc.strftime("%a, %d %b %Y %H:%M:%S GMT"),
-        'Authorization' =>  "NEATOAPP " + signature,
-      },
-      body: '{"reqId":"77", "cmd":"getRobotState"}'
-    )
-  end
-
-  private
-
-  def oauth2
-    @client = OAuth2::Client.new(CLIENT_ID, CLIENT_SECRET_KEY, :site => NEATO_ENDPOINT)
-
-    response = @client.auth_code.authorize_url(:redirect_uri => REDIRECT_URI, :scope => SCOPE)
-
-    response = response.gsub!('/oauth/','/oauth2/')
-  end
-  # move to a new job to refresh every x days
-  # put expiration on token and run this job
-  def refresh_token
-    response = HTTParty.post(API_ENDPOINT,
-    body: {
-      "grant_type": "refresh_token",
-      "refresh_token": REFRESH_TOKEN
-      }
-    )
-  end
-
-  def signature
-    robot_serial = ROBOT_SERIAL
-    date = Time.now.utc.strftime("%a, %d %b %Y %H:%M:%S GMT")
-
-    robot_secret_key = ROBOT_SECRET
-
-    body = '{"reqId":"77", "cmd":"getRobotState"}'
-
-    string_to_sign = "#{robot_serial.downcase}\n#{date}\n#{body}"
-
-    signature = OpenSSL::HMAC.hexdigest('sha256', robot_secret_key, string_to_sign)
-  end
-
-  # move to a new utility maybe or to the access token model
-
-  def get_access_token
-    response = HTTParty.post(API_ENDPOINT,
-      body: {
-        "grant_type": "authorization_code",
-        "client_id": CLIENT_ID,
-        "client_secret": CLIENT_SECRET_KEY,
-        "redirect_uri": REDIRECT_URI,
-        "code": "5625ed0445c3698b6cf9bac7e9070de7a93694c3f27f4f0d64fdfe8a3683b665"
+        'Accept' => HEADER_URL,
+        'Authorization' =>  "Bearer " + "#{ACCESS_TOKEN}",
       }
     )
   end
 end
+
+
+
+
+
+
+
+  # private
+
+  # def oauth2
+  #   @client = OAuth2::Client.new(CLIENT_ID, CLIENT_SECRET_KEY, :site => NEATO_ENDPOINT)
+
+  #   response = @client.auth_code.authorize_url(:redirect_uri => REDIRECT_URI, :scope => SCOPE)
+
+  #   response = response.gsub!('/oauth/','/oauth2/')
+  # end
+  # # move to a new job to refresh every x days
+  # # put expiration on token and run this job
+  # def refresh_token
+  #   response = HTTParty.post(API_ENDPOINT,
+  #   body: {
+  #     "grant_type": "refresh_token",
+  #     "refresh_token": REFRESH_TOKEN
+  #     }
+  #   )
+  # end
+
+  # # move to a new utility maybe or to the access token model
+
+  # def get_access_token
+  #   response = HTTParty.post(API_ENDPOINT,
+  #     body: {
+  #       "grant_type": "authorization_code",
+  #       "client_id": CLIENT_ID,
+  #       "client_secret": CLIENT_SECRET_KEY,
+  #       "redirect_uri": REDIRECT_URI,
+  #       "code": "a9b217c5825b25809f4c4298ad3a42806e8bd42c603bda569b490a7cc7a43004"
+  #     }
+  #   )
+  # end
