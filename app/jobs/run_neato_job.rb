23298ecf95eaa58bda765d1a6804d9773473f4fb
jearbot <heskettjeremy@gmail.com>
2020-06-03 18:24:24 -0500

    spelling mistake

--- app/jobs/run_neato_job.rb
+++ app/jobs/run_neato_job.rb
@@ -5,7 +5,7 @@ class RunNeatoJob < ApplicationJob
     @date = Time.now.utc.strftime("%a, %d %b %Y %H:%M:%S GMT")
 
     get_signature
-    response = HTTParty.post("https://nucleo.neatocloud.com:4443/vendors/neato/robots/#{AcessToken::ROBOT_SERIAL}/messages",
+    response = HTTParty.post("https://nucleo.neatocloud.com:4443/vendors/neato/robots/#{AccessToken::ROBOT_SERIAL}/messages",
       headers: {
         'Accept' => 'application/vnd.neato.nucleo.v1',
         'Date' => @date,
@@ -20,7 +20,7 @@ class RunNeatoJob < ApplicationJob
   def get_signature
     body = JSON.dump({ reqId: "13", cmd: "startCleaning", params: { category: 4, mode: 2, navigationMode: 1, mapId: '2020-03-08T15:36:19Z' }})
 
-    string_to_sign = "#{AcessToken::ROBOT_SERIAL.downcase}\n#{@date}\n#{body}"
+    string_to_sign = "#{AccessToken::ROBOT_SERIAL.downcase}\n#{@date}\n#{body}"
 
     @signature = OpenSSL::HMAC.hexdigest('sha256', AccessToken::ROBOT_SECRET, string_to_sign)
   end
