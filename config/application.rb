216cf8a3fd7e4dd2ea195893a348f263a42a6946
jearbot <heskettjeremy@gmail.com>
2020-06-03 13:48:01 -0500

    more fixes

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ module Neato
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.assets.enabled = false
+    config.assets.compile = false
+    config.assets.js_compressor = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
